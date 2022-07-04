0808d9e763bf474d4af478f5d6ddc309eb882abc
Aigul <sharipova.aig@gmail.com>
2022-07-04 15:20:44 -0400

    all in millisec

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ function christmasCountdown () {
   const diff = christmasDate - now;
 
   const msInSecond = 1000;
-  
+  const msInMinute = 60 * 1000;
+  const msInHour = 60 * 60 * 1000;
+  const msInDay = 24 * 60 * 60 * 1000;
 }
