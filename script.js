e8a8d44339efa8c6bbf4c6ac282d9bc1ab40f244
Aigul <sharipova.aig@gmail.com>
2022-07-04 15:15:07 -0400

    const second

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ function christmasCountdown () {
   const christmasDate = new Date ('December 25, 2021 00:00');
   const now = new Date ();
   const diff = christmasDate - now;
+
+  const msInSecond = 1000;
+  
 }
